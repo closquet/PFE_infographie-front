bb690de53b97ef1d9083c1ebbb4fbc1953d16374
Eric Closquet <closquet.eric@live.be>
2019-08-03 14:43:26 +0200

    add variables settings for css: colors, fonts, spaces, clean Home.vue, clean router.js, removed default views

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app" class="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'home'}">Home</router-link> |
     </div>
     <router-view/>
   </div>
 </template>
+
+<style lang="scss">
+  html {
+    font-family: $labor-text-font-family, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: $color-2;
+    background-color: $color-1;
+  }
+
+  .page-title {
+    font-family: $page-title-font-family;
+  }
+</style>
