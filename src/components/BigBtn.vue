fcefc9e74372628861f3faf135751eead6959fa0
Eric Closquet <closquet.eric@live.be>
2019-08-16 21:20:04 +0200

    edit style and name of btn1 -> BigButton

--- src/components/BigBtn.vue
+++ src/components/BigBtn.vue
@@ -1,6 +1,6 @@
 <template>
     <button @click="$emit('click')"
-            class="btn1">
+            class="big-btn">
         {{text}}
     </button>
 </template>
@@ -15,23 +15,24 @@
 </script>
 
 <style scoped lang="scss">
-    .btn1 {
+    .big-btn {
         color: $color-1;
-        background-color: $color-9;
+        background-color: $color-3;
         display: block;
         width: 100%;
         min-height: 50px;
-        border: 2px solid $color-8;
+        border: 2px solid $color-3;
         border-radius: 3px;
         font-size: $font-size-5;
         transition: 0.3s;
 
         &:hover, &:focus {
-            background-color: $color-8;
+            background-color: $color-1;
+            color: $color-3;
         }
     }
 
-    form .btn1 {
+    form .big-btn {
         margin-top: $vertical-space-size-4;
     }
 </style>
