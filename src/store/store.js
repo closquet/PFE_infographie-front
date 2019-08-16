753526c075f6ca9123da3f5a06eb82ba5aaaaef7
Eric Closquet <closquet.eric@live.be>
2019-08-16 21:29:13 +0200

    create Allergens Store, actions/getters/mutations/state, add getAllAllergens action/mutation

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import user from './user/user.store.js';
+import allergens from './allergens/allergens.store.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules : {
     user,
+    allergens,
   }
 });
